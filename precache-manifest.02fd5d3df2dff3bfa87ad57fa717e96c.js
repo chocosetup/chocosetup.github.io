719d96f21845f8075c6a3950db17f58c7b60cc33
Double-A-92 <amato.amedeo@gmail.com>
2020-08-22 22:42:53 +0200

    Updates

---
+++ precache-manifest.02fd5d3df2dff3bfa87ad57fa717e96c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f21adfb7c673e77a4fec9b9054829afc",
+    "url": "/Double-A-92/chocosetup/index.html"
+  },
+  {
+    "revision": "21dc95f515098e433596",
+    "url": "/Double-A-92/chocosetup/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7daf20435e2e69b23481",
+    "url": "/Double-A-92/chocosetup/static/js/2.ea8d69a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Double-A-92/chocosetup/static/js/2.ea8d69a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21dc95f515098e433596",
+    "url": "/Double-A-92/chocosetup/static/js/main.68ad6191.chunk.js"
+  },
+  {
+    "revision": "d64e4b6367d4b88960ea",
+    "url": "/Double-A-92/chocosetup/static/js/runtime-main.08fe78bd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Double-A-92/chocosetup/static/media/logo.5d5d9eef.svg"
+  }
+]);
