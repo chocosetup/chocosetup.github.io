c70bf94f253e276ae9c8439badb21ce2a3b091f8
Double-A-92 <amato.amedeo@gmail.com>
2021-12-25 22:45:44 +0100

    Add script generator and display

--- src/Generator.js
+++ src/Generator.js
@@ -1,17 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import Highlight from "react-highlight";
+import "highlight.js/styles/atom-one-dark.css";
 import "./Generator.css";
 
 function Generator(props) {
+  const chocolateyInstallCommand = // See: https://chocolatey.org/install#individual
+    "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))";
+
+  const appInstallCommand = "choco install {1} -y";
+
+  const [script, setScript] = useState("");
+
   function logPickedApps() {
-    console.log("Picked apps : ", props.pickedApps);
+    let script = "";
+    script += chocolateyInstallCommand + "\n";
+    props.pickedApps.forEach((app) => {
+      script += appInstallCommand.replace("{1}", app) + "\n";
+    });
+    console.log(script);
+    setScript(script);
+  }
+
+  function shouldDisable() {
+    return props.pickedApps.size === 0;
   }
 
   return (
     <div className="generator">
       <h2>2. Download and run your custom installer</h2>
-      <button className="button" disabled="" onClick={logPickedApps.bind(this)}>
-        <span className="text">Download</span>
+      <button
+        className="button"
+        type="button"
+        disabled={shouldDisable()}
+        onClick={logPickedApps.bind(this)}
+      >
+        <span className="text">Generate Script</span>
       </button>
+      <div className="script-container">
+        <Highlight className="powershell">{script}</Highlight>
+      </div>
     </div>
   );
 }
